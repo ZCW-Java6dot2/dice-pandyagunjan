0967bd3d826fe120864b2e42ca95b1fc2b12bf56
gunjan <gunjan@zipcodes-mbp-78.lan>
2020-11-05 07:45:43 -0500

    Just started

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,23 @@
+import java.util.Random;
+
 public class Dice {
+    Integer diceRolled;
 
+    public Dice(Integer diceRolled) {
+        this.diceRolled=diceRolled;
+    }
 
+    public Integer tosssAndSum()
+    {
+        Integer sum=0;
+        int max=6;
+        int min =1;
+     //   Random random=new Random();
+        for(i=0;i<diceRolled;i++)
+        {
+            sum+=Math.random()*(max-min+1)+min;
+
+        }
+   return sum;
+    }
 }
