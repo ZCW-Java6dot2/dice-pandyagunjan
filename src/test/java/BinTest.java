3ea7ab769d527dff4faee3c8290c140f98463b3a
gunjan <gunjan@zipcodes-mbp-78.lan>
2020-11-08 20:02:42 -0500

    bin test

--- src/test/java/BinTest.java
+++ src/test/java/BinTest.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class BinTest {
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class BinTest {
+
+    @Test
+    public void binsConstructorTest(){
+        Bins bins = new Bins(2,12);
+
+        Integer expected = 0;
+
+        Assert.assertEquals(expected, bins.getBin(10));
+    }
+
+    @Test
+    public void getBinTest(){
+        Bins bin1 = new Bins(2,12);
+        Integer expected = 0;
+
+        Assert.assertEquals(expected, bin1.getBin(12));
+    }
+
+    @Test
+    public void incrementTest(){
+        Bins bin2 = new Bins(2,12);
+        Integer expected = 1;
+        bin2.incrementBin(10);
+
+        Assert.assertEquals(expected, bin2.getBin(10));
+    }
 }
